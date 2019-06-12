baffcc93db177bb8c86ef3c3c15391b9fb8af185
gulifan <gulifan@dxy.cn>
2019-06-12 16:26:59 +0800

    feat:路由元信息

--- src/router/router.js
+++ src/router/router.js
@@ -26,6 +26,9 @@ export default [
     props: {
       food: 'banana'
     },
+    meta: {
+      title: '关于'
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
