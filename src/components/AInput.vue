44ee4b8218a805d556ac1f8f0518e2ff393f0594
gulifan <gulifan@dxy.cn>
2019-06-17 10:27:23 +0800

    feat:父子组件通信

---
+++ src/components/AInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" @input="handleInput" :value="value" />
+</template>
+
+<script>
+export default {
+  name: 'AInput',
+  props: {
+    value: {
+      type: [String, Number]
+    }
+  },
+  methods: {
+    handleInput (event) {
+      const value = event.target.value
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/store.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a-input v-model="inputValue" />
+    <p>{{inputValue}}</p>
+   </div>
+</template>
+
+<script>
+import AInput from '_c/AInput'
+export default {
+  name: 'store',
+  data () {
+    return {
+      inputValue: ''
+    }
+  },
+  components: {
+    AInput
+  }
+}
+</script>
+
+<style>
+
+</style>
