36d578d0c9db5f15aeadf60819a9434ae3d5f1ad
gulifan <gulifan@dxy.cn>
2019-06-17 10:36:33 +0800

    feat:兄弟组件通信

---
+++ src/components/AShow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>AShow: {{ content }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/store.vue
+++ src/views/store.vue
@@ -2,11 +2,13 @@
   <div>
     <a-input v-model="inputValue" />
     <p>{{inputValue}}</p>
+    <a-show :content="inputValue" />
    </div>
 </template>
 
 <script>
 import AInput from '_c/AInput'
+import AShow from '_c/AShow'
 export default {
   name: 'store',
   data () {
@@ -15,7 +17,8 @@ export default {
     }
   },
   components: {
-    AInput
+    AInput,
+    AShow
   }
 }
 </script>
