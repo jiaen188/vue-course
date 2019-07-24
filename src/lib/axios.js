ce2a71acbf2fbef9145bf76eb30949261b298d07
gulifan <gulifan@dxy.cn>
2019-07-24 15:08:40 +0800

    feat: axios封装优化

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -17,17 +17,23 @@ class HttpRequest {
     return config
   }
 
-  interceptors (instance) {
+  interceptors (instance, url) {
     instance.interceptors.request.use(config => {
+      console.log('config', config)
       // 添加全局的loading...
+      if (!Object.keys(this.queue).length) console.log('Spin.show()')// 开始请求
+      this.queue[url] = true
       return config
     }, error => {
       return Promise.reject(error)
     })
     instance.interceptors.response.use(res => {
+      delete this.queue[url]
       console.log('instance.interceptors.response res', res)
-      return res
+      const { data, status } = res
+      return { data, status }
     }, error => {
+      delete this.queue[url]
       return Promise.reject(error)
     })
   }
@@ -35,7 +41,7 @@ class HttpRequest {
   request (options) {
     const instance = axios.create()
     options = Object.assign(this.getInsideConfig(), options)
-    this.interceptors(instance)
+    this.interceptors(instance, options.url)
     return instance(options)
   }
 }
