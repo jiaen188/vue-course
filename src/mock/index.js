ed582988a2c39945abb7bbf51142d698768823a9
gulifan <gulifan@dxy.cn>
2019-07-30 00:10:25 +0800

    feat:mock的基本使用

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import Mock from 'mockjs'
+import { getUserInfo } from './response/user'
 
-//
+Mock.mock('http://localhost:3000/index/getUserInfo', getUserInfo)
 
 export default Mock
---
+++ src/mock/response/user.js
@@ -0,0 +1,4 @@
+export const getUserInfo = (options) => {
+  console.log('mock options', options)
+  return { name: 'Gavin' }
+}
