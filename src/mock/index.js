4a29fa93b74b7c0ecfc401157d50f7a3507b61a9
gulifan <gulifan@dxy.cn>
2019-07-30 15:20:22 +0800

    feat:mock模板数据类型

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,6 @@
 import Mock from 'mockjs'
 import { getUserInfo } from './response/user'
-
+// Mock.mock(/\/index\/getUserInfo/, { name: 'Gavin' })
 Mock.mock('http://localhost:3000/index/getUserInfo', getUserInfo)
 
 export default Mock
--- src/mock/response/user.js
+++ src/mock/response/user.js
@@ -1,4 +1,30 @@
+import Mock from 'mockjs'
+
+// export const getUserInfo = (options) => {
+//   console.log('mock options', options)
+//   return { name: 'Gavin' }
+// }
+
 export const getUserInfo = (options) => {
-  console.log('mock options', options)
-  return { name: 'Gavin' }
+  const template = {
+    'str|2-4': 'Gavin',
+    'name|5': 'gavin',
+    'age|+2': 18,
+    'num|4-10': 0,
+    'float|3-10.2-5': 0,
+    'bool|1': true,
+    'bool2|1-9': true, // true的概率 min/ (min+max)
+    'obj|2': { a: 'aa', b: 'bb', c: 'cc' },
+    'obj2|1-2': { a: 'aa', b: 'bb', c: 'cc' },
+    'arr|2-4': [1, 2, 3],
+    'arr2|2': [1, 2, 3],
+    'func': () => 'this is created by funciton',
+    'reg': /[1-9][a-z]/
+  }
+  let i = 3
+  let arr = []
+  while (i--) {
+    arr.push(template)
+  }
+  return Mock.mock(arr)
 }
