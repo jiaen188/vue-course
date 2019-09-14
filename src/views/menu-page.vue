de81d236a961cb067c91807a82de19e7064eb60c
gulifan <gulifan@dxy.cn>
2019-09-14 16:54:41 +0800

    fix:递归组件

--- src/views/menu-page.vue
+++ src/views/menu-page.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="menu-box">
-    <a-menu>
+    <!-- <a-menu>
       <a-menu-item>111</a-menu-item>
       <a-menu-item>222</a-menu-item>
       <a-submenu>
@@ -12,6 +12,12 @@
           <a-menu-item>333-22-2</a-menu-item>
         </a-submenu>
       </a-submenu>
+    </a-menu> -->
+    <a-menu>
+      <template v-for="(item, index) in list">
+        <a-menu-item v-if="!item.children" :key="index">{{ item.title }}</a-menu-item>
+        <re-submenu v-else :key="index" :parent="item" :index="index"></re-submenu>
+      </template>
     </a-menu>
   </div>
 </template>
@@ -19,17 +25,54 @@
 <script>
 // import { AMenu, AMenuItem, ASubmenu } from '_c/menu'
 import menuComponents from '_c/menu'
+import ReSubmenu from './re-submenu'
 const { AMenu, AMenuItem, ASubmenu } = menuComponents
 
 export default {
   data() {
     return {
+      list: [
+        {
+          title: '1111'
+        },
+        {
+          title: '2222'
+        },
+        {
+          title: '3333',
+          children: [
+            {
+              title: '3333-1'
+            },
+            {
+              title: '3333-2',
+              children: [
+                {
+                  title: '3333-2-1'
+                },
+                {
+                  title: '3333-2-2'
+                },
+                {
+                  title: '3333-2-3',
+                  children: [
+                    {
+                      title: '3333-2-3-1'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   },
   components: {
     AMenu,
     AMenuItem,
-    ASubmenu
+    ASubmenu,
+    ReSubmenu
   }
 }
 </script>
---
+++ src/views/re-submenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-submenu>
+    <div slot="title">{{ parent.title}}</div>
+    <template v-for="(item, i) in parent.children">
+      <a-menu-item v-if="!item.children" :key="`${i}_${index}`">{{ item.title }}</a-menu-item>
+      <re-submenu v-else :key="`${i}_${index}`" :parent="item"></re-submenu>
+    </template>
+  </a-submenu>
+</template>
+
+<script>
+import menuComponents from '_c/menu'
+const { AMenuItem, ASubmenu } = menuComponents
+
+export default {
+  name: 'ReSubmenu',
+  props: {
+    parent: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    index: Number
+  },
+  components: {
+    AMenuItem,
+    ASubmenu
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
