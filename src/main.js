3c6c7630b62646add735103e084766d4e915b539
gulifan <gulifan@dxy.cn>
2019-09-18 22:40:49 +0800

    feat:引入iview

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Bus from './lib/bus'
+import iview from 'iview'
+import 'iview/dist/styles/iview.css'
 // import Mock from './mock' // 我们只希望在开发的时候引入mock
 // if (process.env.NODE_ENV !== 'production') require('./mock')
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = Bus
+Vue.use(iview)
 
 const handleClick = event => {
   console.log('event click', event)
